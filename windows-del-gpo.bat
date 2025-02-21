5e85740ad3002b4cf112dc71cb8fecb7cf88de23
panjiansheng <bk20112370@my.swjtu.edu.cn>
2025-02-21 15:46:38 +0800

    Update windows-del-gpo.bat

--- windows-del-gpo.bat
+++ windows-del-gpo.bat
@@ -8,9 +8,9 @@ set "files[2]=%windir%\System32\GroupPolicy\Machine\Scripts\scripts.ini"
 for %%i in (1 2) do (
     set "ini=!files[%%i]!"
     if exist "!ini!.orig" (
-        move /y "!ini!.orig" "!ini!"
+
     ) else (
-        del "!ini!"
+
     )
 )
 
